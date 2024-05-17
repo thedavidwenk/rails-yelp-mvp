1eba1f334b34b9c9d0bf64165816612fdc623d44
David Wenk <davidwenk@icloud.com>
2024-05-17 17:37:37 +0200

    add review option

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -19,6 +19,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @reviews = @restaurant.reviews
   end
 
   private
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,23 @@
 class ReviewsController < ApplicationController
   def new
-  end
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end 
 
   def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurants_path(@restaurant)
+    else
+      render :new, status: 422
+    end
   end
+
+    private
+
+    def review_params
+      params.require(:review).permit(:rating, :content)
+    end
 end
