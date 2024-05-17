8aeaec09b8b0637f4d8ae7b3e77b6be2833dbc8d
David Wenk <davidwenk@icloud.com>
2024-05-17 12:33:39 +0200

    add show all restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,30 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: "Restaurant was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
   end
 end
